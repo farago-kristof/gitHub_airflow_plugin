99b3143873fad094458f4dd5b2af0bc747b192a6
kristof-farago <kristof.farago@nixs.com>
2024-12-11 10:42:55 +0100

    Updated example_dag.py

--- examples/dags/example_dag.py
+++ examples/dags/example_dag.py
@@ -7,7 +7,9 @@ from operators.repo import GitHubPullRequestOperator
 from operators.repo import GitHubIssuesOperator
 from operators.repo import GitHubBranchesOperator
 from operators.repo import GitHubCommitsOperator
+from airflow.operators.python import PythonOperator
 
+# Default arguments for DAG and tasks
 default_args = {
     'owner': 'airflow',
     'start_date': datetime(2024, 12, 1),
@@ -20,11 +22,21 @@ default_task_args = {
     'repo_name': 'fugro_3'
 }
 
+def log_xcom(**kwargs):
+    """
+    Logs the XCom pushed by the previous task.
+    :param kwargs: Context variables provided by Airflow.
+    """
+    task_instance = kwargs['ti']
+    task_id = kwargs['task_id']
+    xcom_value = task_instance.xcom_pull(task_ids=task_id)
+    print(f"XCom value from task {task_id}: {xcom_value}")
+
 with DAG('github_dag', default_args=default_args, schedule_interval=None) as dag:
     authenticate = GitHubAuthOperator(
         task_id=default_task_args['auth_task_id'],
         session_name=default_task_args['session_name'],
-        token='{{ var.value.github_token }}'
+        conn_id='github_conn_id'
     )
 
     fetch_pull_requests = GitHubPullRequestOperator(
@@ -33,9 +45,23 @@ with DAG('github_dag', default_args=default_args, schedule_interval=None) as dag
         **default_task_args
     )
 
+    log_pull_requests = PythonOperator(
+        task_id='log_pull_requests',
+        python_callable=log_xcom,
+        provide_context=True,
+        op_kwargs={'task_id': 'fetch_pull_requests'}
+    )
+
     fetch_branches = GitHubBranchesOperator(
         task_id='fetch_branches',
         **default_task_args
+    )
+
+    log_branches = PythonOperator(
+        task_id='log_branches',
+        python_callable=log_xcom,
+        provide_context=True,
+        op_kwargs={'task_id': 'fetch_branches'}
     )
 
     fetch_commits = GitHubCommitsOperator(
@@ -44,10 +70,28 @@ with DAG('github_dag', default_args=default_args, schedule_interval=None) as dag
         **default_task_args
     )
 
+    log_commits = PythonOperator(
+        task_id='log_commits',
+        python_callable=log_xcom,
+        provide_context=True,
+        op_kwargs={'task_id': 'fetch_commits'}
+    )
+
     fetch_issues = GitHubIssuesOperator(
         task_id='fetch_issues',
         assignee='farad213',
         **default_task_args
     )
 
+    log_issues = PythonOperator(
+        task_id='log_issues',
+        python_callable=log_xcom,
+        provide_context=True,
+        op_kwargs={'task_id': 'fetch_issues'}
+    )
+
     authenticate >> [fetch_pull_requests, fetch_branches, fetch_commits, fetch_issues]
+    fetch_pull_requests >> log_pull_requests
+    fetch_branches >> log_branches
+    fetch_commits >> log_commits
+    fetch_issues >> log_issues
--- src/operators/auth.py
+++ src/operators/auth.py
@@ -3,18 +3,24 @@ import typing as t
 
 from airflow.models import BaseOperator
 from airflow.utils.decorators import apply_defaults
+from airflow.hooks.base import BaseHook
 
 from github.auth import GitHubSessionPool
 
 
 class GitHubAuthOperator(BaseOperator):
     @apply_defaults
-    def __init__(self, token: str, session_name: str, **kwargs) -> None:
+    def __init__(self, conn_id, session_name: str, **kwargs) -> None:
         super().__init__(**kwargs)
-        self.token = token
+        self.token = self.retrieve_token(conn_id)
         self.session_name = session_name
 
     def execute(self, context: t.Dict) -> None:
         session = GitHubSessionPool().get_session(self.token)
         ti = context['ti']
         ti.xcom_push(key=self.session_name, value=pickle.dumps(session))
+
+    @staticmethod
+    def retrieve_token(conn_id):
+        conn = BaseHook.get_connection(conn_id)
+        return conn.password
